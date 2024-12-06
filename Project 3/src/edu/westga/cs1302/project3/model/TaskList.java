ac0736227e9c855f245498b81b91af0367165b4b
Mason Gauge Richardson <mricha23@my.westga.edu>
2024-12-05 19:10:23 -0500

    Task 1B complete
    class TaskList added that allows the collection of Task objects into an arrayList.

---
+++ Project 3/src/edu/westga/cs1302/project3/model/TaskList.java
@@ -0,0 +1,13 @@
+package edu.westga.cs1302.project3.model;
+
+/**Wrapper class for the class "Task" 
+ * 
+ * this class aims to allow for the collection of task. 
+ * 
+ * @author mricha23
+ * @version Fall 2024
+ * 
+ */
+public class TaskList {
+
+}
